eff1a46c52c99b774ce208bd9a14545593ae8c85
cyberdevlk <138901572+cyberdevlk@users.noreply.github.com>
2024-08-16 09:33:07 +0530

    Create list04.js

---
+++ cscloud01/list04.js
@@ -0,0 +1,12 @@
+{
+"replaceDomain" : "https://cscloudv2-84fbc9ad93c4.herokuapp.com",
+"buttons": {
+     "tg":"/tg",
+     "direct" : "/swatch"
+},
+"replacement" : [
+     {"from":"/tnganuwpxppzfymytsqr/202408","to":"/2235074391"},
+     {"from":"/tnganuwpxppzfymytsqr/202403","to":"/2235074392"},
+     {"from":"/tnganuwpxppzfymytsqr/012024","to":"/2235074393"}
+]
+}
