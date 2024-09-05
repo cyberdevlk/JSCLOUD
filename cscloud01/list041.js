4368d776d4acd7e536167c162099e70d8c4abe89
cyberdevlk <138901572+cyberdevlk@users.noreply.github.com>
2024-09-05 10:30:34 -0700

    Update list041.js

--- cscloud01/list041.js
+++ cscloud01/list041.js
@@ -6,7 +6,9 @@
 },
 "replacement" : [
      {"from":"/tnganuwpxppzfymytsqr/202409","to":"/2235074391"},
-     {"from":"/tnganuwpxppzfymytsqr/202410","to":"/2235074392"},
+     {"from":"/qmsyfzbjcavekxfuwqbi/TV%20Series/Game%20Of%20Thrones","to":"/2245252603"},
+     {"from":"/qmsyfzbjcavekxfuwqbi/TV%20Series/Breaking.Bad%20S01-05","to":"/2245252603"},
+    {"from":"/qmsyfzbjcavekxfuwqbi/TV%20Series/Merlin","to":"/2245252603"},
      {"from":"/tnganuwpxppzfymytsqr/202411","to":"/2235074393"}
 ]
 }
